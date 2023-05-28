406f329487fbc565935fa1524081b8d5e9301314
Muhammad Ridho Adhari <110064566+Jnosvb@users.noreply.github.com>
2023-05-28 23:08:25 +0700

    ths
    ths

---
+++ frontend/section/footer.php
@@ -0,0 +1,37 @@
+    <!-- Contact Us -->
+    <div id="contact" class="contact-us">
+      <div class="title">
+        <h1>Contact Us</h1>
+        <p>Contact us with any suggestions and input for our community</p>
+        <a href="https://www.instagram.com/lvn.jenn/" class="instagram"><i class="fa-brands fa-instagram fa-2xl"></i></a>
+        <a href=""><i class="fa-regular fa-envelope fa-2xl"></i></a>
+        <a href=""><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
+      </div>
+      <form action="">
+        <div class="contact-form">
+          <label for="email">Email</label> <br />
+          <input type="text" name="email_from" id="email" placeholder="Masukkan Email Anda" require/>
+        </div>
+
+        <div class="contact-form">
+          <label for="email">Nama</label> <br />
+          <input type="text" name="nama" id="nama" placeholder="Masukkan Nama Anda"/>
+        </div>
+
+        <div class="contact-form">
+          <label for="email">Pesan</label> <br />
+          <textarea name="pesan" id="" rows="12" placeholder="Masukkan Pesan Anda" ></textarea>
+        </div>
+        <button name="submit" type="submit">Kirim Pesan</button>
+      </form>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer">
+      <h5>UNIVERSITAS BINA SARANA INFORMATIKA KOTA PONTIANAK</h5>
+    </div>
+
+    <script src="script/index.js"></script>
+    
+  </body>
+</html>
